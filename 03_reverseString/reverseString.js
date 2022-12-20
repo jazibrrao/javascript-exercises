8dd517fad9837447912b7116989122006a592e95
Sp1ke <jazibrrao@gmail.com>
2022-12-20 18:55:44 -0500

    exercise 03 - reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const splitStr = str.split('');
+    const reverseArr = splitStr.reverse();
+    const joinArr = reverseArr.join('');
+    return joinArr;
 };
 
 // Do not edit below this line
