bbf65299ef4177535db672524fec9ddcb988fe91
Sp1ke <jazibrrao@gmail.com>
2022-12-20 18:41:42 -0500

    exercise 08 - calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -3,23 +3,31 @@ const add = function(a,b) {
 };
 
 const subtract = function(a,b) {
-	
+	return a - b;
 };
 
-const sum = function() {
-	
+
+//take array and use .reduce to combine all elemnts into one
+const sum = function(arr) {
+  return arr.reduce((total, currentValue) => total + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, currentValue) => total * currentValue)
 };
 
-const power = function() {
-	  
+const power = function(a,b) {
+	  return a**b;
 };
 
-const factorial = function() {
-	
+//factorial = product of all positive numbers less than or equal to number (0! = 1)
+const factorial = function(n) {
+  let result = 1;
+  while(n > 0) {
+    result *= n;
+    n--;
+  }
+  return result;
 };
 
 // Do not edit below this line
