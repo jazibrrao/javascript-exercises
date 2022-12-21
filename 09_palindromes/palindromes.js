c4281a56a5eda3b84889abb5261b65dce1aeba25
Sp1ke <jazibrrao@gmail.com>
2022-12-20 19:17:17 -0500

    exercise 09 - palindrome checker

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // convert to lowercase
+    str = str.toLowerCase();
 
+    // remove all punctuation
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    // remove all whitespace
+    str = str.replace(/\s/g, "");
+
+    const splitStr = str.split('');
+    const reverseArr = splitStr.reverse();
+    const reversedStr = reverseArr.join('');
+
+    return str === reversedStr;
 };
 
 // Do not edit below this line
