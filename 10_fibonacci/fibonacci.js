daddb7da4b61e6891c3825c313c21953079acdc1
Sp1ke <jazibrrao@gmail.com>
2022-12-21 16:28:19 -0500

    exercise 10 - fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+//fibonacci: series of numbers where new number = sum of preceding two numbers
+const fibonacci = function(n) {
+    let num = parseInt(n);
+    let oldVal = 0; //first number in series
+    let newVal = 1; //second number in series
+    let temp;
+    while(num > 1) {
+        temp = newVal;
+        newVal = oldVal + newVal;
+        oldVal = temp;
+        num--;
+    }
+    return num < 0 ? "OOPS" : newVal; // if num is negative, return oops, else value of nth number in series
 };
 
 // Do not edit below this line
